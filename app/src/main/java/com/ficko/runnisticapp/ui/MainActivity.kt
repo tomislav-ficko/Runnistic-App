c96eb17b6ba50583ce3ac78ac8967e75de8762ef
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-23 15:27:34 +0200

    Define navigation between fragments using Navigation Components and link bottom nav menu functionality

--- app/src/main/java/com/ficko/runnisticapp/ui/MainActivity.kt
+++ app/src/main/java/com/ficko/runnisticapp/ui/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.ficko.runnisticapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ficko.runnisticapp.R
-import com.ficko.runnisticapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar) // Tells Android that our main toolbar is "toolbar"
+
+        val navigationController = navHostFragment.findNavController()
+        // Connecting bottomNavigationView to Navigation Components
+        // Whenever an item is clicked on the bottomNavigationView, Android will navigate to the selected fragment
+        bottomNavigationView.setupWithNavController(navigationController)
+
+        // Hiding the bottomNavigationView when the SetupFragment and TrackingFragment are visible
+        // The listener will be triggered whenever our destination has changed using Navigation Components
+        navigationController.addOnDestinationChangedListener { _, destination, _ -> // If we won't use an argument, we can put an underscore instead of the argument name
+            when (destination.id) {
+                R.id.setupFragment, R.id.trackingFragment ->
+                    bottomNavigationView.visibility = View.GONE
+                else ->
+                    bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/ficko/runnisticapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/ficko/runnisticapp/ui/fragments/RunFragment.kt
@@ -1,14 +1,26 @@
 package com.ficko.runnisticapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ficko.runnisticapp.R
 import com.ficko.runnisticapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     // Since Dagger manages the ViewModelFactory behind the scenes, it will choose the appropriate ViewModel from "viewModels()" and assign it to the variable
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
