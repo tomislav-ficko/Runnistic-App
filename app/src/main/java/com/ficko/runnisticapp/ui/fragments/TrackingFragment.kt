e2ba077d0b583d3472154cd0ed6689ea7acedac7
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-25 20:02:38 +0200

    Handle MapView's lifecycle

--- app/src/main/java/com/ficko/runnisticapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/ficko/runnisticapp/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,59 @@
 package com.ficko.runnisticapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ficko.runnisticapp.R
 import com.ficko.runnisticapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    // We need to worry about the lifecycle of our MapView, which is why we need to override the default methods
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+
+        mapView?.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
