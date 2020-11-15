55e90370717c45fcd3e4b23c9598a5ff552508e9
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-15 20:40:37 +0100

    Add functionality for first app startup (save name and weight)

--- app/src/main/java/com/ficko/runnisticapp/di/AppModule.kt
+++ app/src/main/java/com/ficko/runnisticapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.ficko.runnisticapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.ficko.runnisticapp.db.Database
 import com.ficko.runnisticapp.other.Constants.DATABASE_NAME
+import com.ficko.runnisticapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.ficko.runnisticapp.other.Constants.KEY_NAME
+import com.ficko.runnisticapp.other.Constants.KEY_WEIGHT
+import com.ficko.runnisticapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,6 +20,25 @@ import javax.inject.Singleton
 @Module // Tells Dagger that this will be our Module object
 @InstallIn(ApplicationComponent::class) // Because of this annotation, the objects created within AppModule will live until the whole application is destroyed
 object AppModule { // The purpose of AppModule is to tell Dagger how to create certain objects (which it wouldn't know how to do otherwise)
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPrefs: SharedPreferences) = sharedPrefs.getString(KEY_NAME, "") ?: ""
+    // getString sometimes returns null, that's why we do an additional null-check
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPrefs: SharedPreferences) = sharedPrefs.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPrefs: SharedPreferences) =
+        sharedPrefs.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 
     @Singleton
     @Provides
@@ -31,6 +56,6 @@ object AppModule { // The purpose of AppModule is to tell Dagger how to create c
     // Instruction on how to create the instance of DAO
     fun provideRunDao(db: Database) = db.getRunDao()
 
-        // We don't need to explicitly write a 'provide' function for our MainRepository because Dagger will automatically know how to create it,
-        // since it knows how to create a RunDAO object, and this is the only dependency of the MainRepository class
+    // We don't need to explicitly write a 'provide' function for our MainRepository because Dagger will automatically know how to create it,
+    // since it knows how to create a RunDAO object, and this is the only dependency of the MainRepository class
 }
--- app/src/main/java/com/ficko/runnisticapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/ficko/runnisticapp/ui/fragments/TrackingFragment.kt
@@ -29,6 +29,7 @@ import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 import java.util.*
+import javax.inject.Inject
 import kotlin.math.round
 
 @AndroidEntryPoint
@@ -44,7 +45,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private var menu: Menu? = null
 
-    private var weight = 80f
+    @set:Inject
+    var weight = 80f
+    // Cannot be lateinit var since boolean is a primitive data type, so it also cannot be normal Inject annotation
 
     override fun onCreateView(
         inflater: LayoutInflater,
