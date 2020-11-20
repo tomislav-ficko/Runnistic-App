174ca6403d05922dcfad85c827ef9cfde0303d48
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-20 14:13:31 +0100

    Set up BarChart for visualizing run data

--- app/src/main/java/com/ficko/runnisticapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/ficko/runnisticapp/ui/fragments/StatisticsFragment.kt
@@ -1,13 +1,20 @@
 package com.ficko.runnisticapp.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.ficko.runnisticapp.R
+import com.ficko.runnisticapp.other.CustomMarkerView
 import com.ficko.runnisticapp.other.TrackingUtility
 import com.ficko.runnisticapp.ui.viewmodels.StatisticsViewModel
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_statistics.*
 import java.lang.Math.round
@@ -19,7 +26,33 @@ class StatisticsFragment : Fragment(R.layout.fragment_settings) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         subscribeToObservers()
+        setupBarChart()
+    }
+
+    private fun setupBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisRight.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.apply {
+            description.text = "Average Speed Over Time"
+            legend.isEnabled = false
+        }
     }
 
     private fun subscribeToObservers() {
@@ -51,6 +84,21 @@ class StatisticsFragment : Fragment(R.layout.fragment_settings) {
             it?.let {
                 val totalCalories = "${it}kcal"
                 tvTotalCalories.text = totalCalories
+            }
+        })
+
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeeds = it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeed) }
+                val barDataSet = BarDataSet(allAvgSpeeds, "Average Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                }
+                barChart.data = BarData(barDataSet)
+                // Display pop-up window when clicking on a bar, with details about the run
+                barChart.marker =
+                    CustomMarkerView(it.reversed(), requireContext(), R.layout.marker_view)
+                barChart.invalidate() // Draw chart again with new data
             }
         })
     }
